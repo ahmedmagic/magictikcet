2b5b6756e36f7efc34355d0dd7a2b7ff36b721e3
ahmedmagic <45693130+ahmedmagic@users.noreply.github.com>
2018-12-18 15:00:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const moment = require("moment");
-const prefix = '-'
+const prefix = '#'
 
 
 client.on('ready', () => {
